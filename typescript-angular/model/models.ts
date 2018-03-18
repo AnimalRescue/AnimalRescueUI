44e65f091c5e9a58f40e2476532b0e47df84f5fe
jgartee <jlgartee@gmail.com>
2018-03-18 14:45:39 +0000

    Generated code by SwaggerHub

--- typescript-angular/model/models.ts
+++ typescript-angular/model/models.ts
@@ -1,3 +1,6 @@
 export * from './adoptionStatus';
+export * from './animalBreeds';
+export * from './catBreeds';
+export * from './dogBreeds';
 export * from './inventoryItem';
 export * from './manufacturer';
